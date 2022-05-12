f55a02a67772aea4ee8770c169ad119e83a4268e
Polina Ka <polinako1994@gmail.com>
2022-05-12 14:16:37 +0900

    Update index.js

--- index.js
+++ index.js
@@ -1,16 +1,37 @@
-let popup = document.querySelector('.popup');
-let popupContainer = document.querySelector('.popup__container'); 
-let openPopupButtons = document.querySelectorAll('.open-popup'); 
-let closePopupButton = document.querySelector('.popup_close'); 
+const popup = document.querySelector('.popup');
+const popupContainer = document.querySelector('.popup__container'); 
+const closePopupButton = document.querySelector('.popup_close'); 
+const openPopupButtons = document.querySelector('.open-popup');
+const profileName = document.querySelector('.profile__info_name')
+const profileJob = document.querySelector('.profile__info_job')
+const inputName = document.querySelector('.input__name')
+const inputJob = document.querySelector('.input__job')
+//открытие попапа
 
-openPopupButtons.forEach((button) => { 
-    button.addEventListener('click', (e) => { 
-        e.preventDefault(); 
-        popup.classList.add('active'); 
-        popupContainer.classList.add('active'); 
-    })
-});
-closePopupButton.addEventListener('click',() => { 
-    popup.classList.remove('active'); 
-    popupContainer.classList.remove('active');
-});
+function openPopup() {
+    popup.classList.add('popup_opened')
+    popupContainer.classList.add('popup__container_opened')
+};
+openPopupButtons.addEventListener('click', openPopup)
+// закрытие попапа
+function closePopup() {
+    popup.classList.remove('popup_opened')
+}
+closePopupButton.addEventListener('click', closePopup)
+
+//редактирование профиля
+function editProfile() {
+    inputName.value = profileName.textContent
+    inputJob.value = profileJob.textContent
+}
+editProfile();
+
+//сохранение информации
+
+function formSubmitHandler(evt) {
+    evt.preventDefault();
+    profileName.textContent = inputName.value;
+    profileJob.textContent = inputJob.value;
+    closePopup();
+}
+popupContainer.addEventListener('submit', formSubmitHandler)
