d1fe6002bca7ca23d5ffb5f5b5bbbcc7315f1b0b
Polina Ka <polinako1994@gmail.com>
2022-05-17 11:58:17 +0900

    Update index.js

--- scripts/index.js
+++ scripts/index.js
@@ -1,37 +1,30 @@
 const popup = document.querySelector('.popup');
-const popupContainer = document.querySelector('.popup__container'); 
 const closePopupButton = document.querySelector('.popup__close-button'); 
 const openPopupButtons = document.querySelector('.open-popup');
 const profileName = document.querySelector('.profile__name')
 const profileJob = document.querySelector('.profile__job')
-const inputName = document.querySelector('.popup__input_name_value')
-const inputJob = document.querySelector('.popup__input_job_value')
-//открытие попапа
+const inputName = document.querySelector('.popup__input_value_name')
+const inputJob = document.querySelector('.popup__input_value_job')
 
+
+//открытие попапа
 function openPopup() {
-    popup.classList.add('popup_opened')
-    popupContainer.classList.add('popup__container_opened')
+    popup.classList.add('popup__opened')
+    inputName.value = profileName.textContent
+    inputJob.value = profileJob.textContent
 };
 openPopupButtons.addEventListener('click', openPopup)
 // закрытие попапа
 function closePopup() {
-    popup.classList.remove('popup_opened')
+    popup.classList.remove('popup__opened')
 }
 closePopupButton.addEventListener('click', closePopup)
 
-//редактирование профиля
-function editProfile() {
-    inputName.value = profileName.textContent
-    inputJob.value = profileJob.textContent
-}
-editProfile();
-
 //сохранение информации
-
 function formSubmitHandler(evt) {
     evt.preventDefault();
     profileName.textContent = inputName.value;
     profileJob.textContent = inputJob.value;
     closePopup();
 }
-popupContainer.addEventListener('submit', formSubmitHandler)
+popup.addEventListener('submit', formSubmitHandler)
