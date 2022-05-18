74a51afb99ebcadb61544c6d70cb9f17d1dd0f9e
Polina Ka <polinako1994@gmail.com>
2022-05-18 09:18:05 +0900

    Update index.js

--- scripts/index.js
+++ scripts/index.js
@@ -1,4 +1,5 @@
 const popup = document.querySelector('.popup');
+const popupForm = document.querySelector('.popup__form')
 const closePopupButton = document.querySelector('.popup__close-button'); 
 const openPopupButtons = document.querySelector('.open-popup');
 const profileName = document.querySelector('.profile__name')
@@ -9,14 +10,14 @@ const inputJob = document.querySelector('.popup__input_value_job')
 
 //открытие попапа
 function openPopup() {
-    popup.classList.add('popup__opened')
+    popup.classList.add('popup_opened')
     inputName.value = profileName.textContent
     inputJob.value = profileJob.textContent
 };
 openPopupButtons.addEventListener('click', openPopup)
 // закрытие попапа
 function closePopup() {
-    popup.classList.remove('popup__opened')
+    popup.classList.remove('popup_opened')
 }
 closePopupButton.addEventListener('click', closePopup)
 
@@ -27,4 +28,4 @@ function formSubmitHandler(evt) {
     profileJob.textContent = inputJob.value;
     closePopup();
 }
-popup.addEventListener('submit', formSubmitHandler)
+popupForm.addEventListener('submit', formSubmitHandler)
