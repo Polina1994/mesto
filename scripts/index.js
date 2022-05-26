c1119bd2db2ab9b45c2477c3ffd10860b268f66a
Polina Ka <polinako1994@gmail.com>
2022-05-26 09:19:48 +0900

    Update index.js

--- scripts/index.js
+++ scripts/index.js
@@ -1,43 +1,78 @@
-const popup = document.querySelector('.popup');
-const popupForm = document.querySelector('.popup__form')
-const closePopupButton = document.querySelector('.popup__close-button'); 
-const openPopupButtons = document.querySelector('.open-popup');
-const profileName = document.querySelector('.profile__name')
-const profileJob = document.querySelector('.profile__job')
-const inputName = document.querySelector('.popup__input_value_name')
-const inputJob = document.querySelector('.popup__input_value_job')
+const popupEdit = document.querySelector('.popup-edit');   //попап редактирования
+const popupAdd = document.querySelector('.popup-add');  //попап добавления
+const popup = document.querySelectorAll('.popup');  //попап
+const popupImage = document.querySelectorAll('.popup-image')
+const popupForm = document.querySelector('.popup__form')  //попап форма
 
+const addButton = document.querySelector('.profile__add-button')  //кнопка ред
+const editButton = document.querySelector('.profile__edit-button') //кнопка добав
+const editPopupCloseButton = document.querySelector('.popup-edit__close-button');  //кнопка закрытия 
+const addPopupCloseButton = document.querySelector('.popup-add__close-button');  //кнопка закрытия 
+const openPopupButtons = document.querySelector('.open-popup');  //кнопка открытия
+
+const profileName = document.querySelector('.profile__name')  //имя
+const profileJob = document.querySelector('.profile__job')  //профессия
+
+const inputName = document.querySelector('.popup__input_value_name')  //имя в форме
+const inputJob = document.querySelector('.popup__input_value_job')  //профессия в форме
+const inputPlace = document.querySelector('.popup__input_value_place')  //название места карточки в форме
+const inputLink = document.querySelector('.popup__input_value_link')  //картинка карточки в форме
+
+const initialCardList = document.querySelector('.elements') //карточки
+const elementTemplate = document.querySelector('#element-template').content; 
 
 //открытие попапа
-function openEditPopup() {
-    popup.classList.add('popup_is-opened')
-    inputName.value = profileName.textContent
-    inputJob.value = profileJob.textContent
-};
-openPopupButtons.addEventListener('click', openEditPopup)
+function openPopup(popup) {
+  popup.classList.add('popup_is-opened')
+}
 
+//открытие попапа добавления
+addButton.addEventListener('click', function () {
+  openPopup(popupAdd);
+})
+
+//открытие попапа редактирования
+editButton.addEventListener('click',function() {
+inputName.value = profileName.textContent
+inputJob.value = profileJob.textContent
+openPopup(popupEdit)
+})
 
 // закрытие попапа
-function closePopup() {
+function closePopup(popup) {
     popup.classList.remove('popup_is-opened')
 }
-closePopupButton.addEventListener('click', closePopup)
+
+editPopupCloseButton.addEventListener('click', function() {
+  closePopup(popupEdit)
+})
+
+addPopupCloseButton.addEventListener('click', function() {
+  closePopup(popupAdd)
+})
 
 //сохранение информации
 function formSubmitHandler(evt) {
     evt.preventDefault();
     profileName.textContent = inputName.value;
     profileJob.textContent = inputJob.value;
-    closePopup();
+    closePopup(popupEdit);
 }
 popupForm.addEventListener('submit', formSubmitHandler)
 
 
-// добавление карточек
+//добавление новой карточки
 
+function formAddHandler(evt) {
+  evt.preventDefault();
+  const card = {}
+  card.name = inputPlace.value
+  card.link = inputLink.value
+  formAddHandler.prepend(renderCard(evt))
+  closePopup(popupAdd)
+}
 
-const initialCardList = document.querySelector('.elements')
-const elementTemplate = document.querySelector('#element-template').content;
+// создание карточек в html
 
 const initialCards = [
     {
@@ -67,32 +102,32 @@ const initialCards = [
   ];
 
 
-  const elementInfo = initialCards.map(function (item) {
-      return {
-          name: item.name,
-          link: item.link
-      }
-  })
+    //функция добавления карточек
+function render(cards) {
+    cards.forEach((item) => {
+      initialCardList.append(renderCard(item))
+    })
+}
+//карточка
+function renderCard({name, link}) {
+    const placeElement = elementTemplate.querySelector('.element').cloneNode(true)
 
-  function render() {
-      elementInfo.forEach(renderCard)
-  }
-  function renderCard({name, link}) {
-      const placeElement = elementTemplate.querySelector('.element').cloneNode(true)
+    placeElement.querySelector('.element__title').textContent = name
+    placeElement.querySelector('.element__image').src = link
 
-      placeElement.querySelector('.element__title').textContent = name
-      placeElement.querySelector('.element__image').src = link
-
-      placeElement.querySelector('.element__like').addEventListener('click', function(evt) {
-        evt.target.setAttribute('active', true)
+    placeElement.querySelector('.element__like').addEventListener('click', function(evt) {
+      evt.target.classList.toggle('element__like-active')  //лайк
     })
 
-        initialCardList.prepend(placeElement)
-  }
-  render()
+    return placeElement
+}
+render(initialCards)
 
-    //лайк 
-
-  
+//удаление
+function deleteCard(evt) {
+  const card = evt.target.closest('card')
+  card.remove();
+}
+// лайк
 
 
