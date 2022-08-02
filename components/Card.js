88d2d99718eba6d71c02cb5f0daef961abd108e6
Polina Ka <polinako1994@gmail.com>
2022-08-02 14:51:36 +0900

    Update Card.js

--- components/Card.js
+++ components/Card.js
@@ -1,23 +1,23 @@
 export class Card {
     constructor(data, cardSelector, handleCardClick) {
         this._link = data.link
-        this._name = data.name
+        this._place = data.place
         this._cardSelector = cardSelector
         this._card = this._getTemplate()
         this._handleCardClick = handleCardClick
-        this._cardName = this._card.querySelector('.element__title')
+        this._cardPlace = this._card.querySelector('.element__title')
         this._cardLink = this._card.querySelector('.element__image')
         this._likeBtn = this._card.querySelector('.element__like')
         this._trashBtn = this._card.querySelector('.element__trash')
     }
 
     _getTemplate() {
-        const card = document
+        const cardElement = document
         .querySelector(this._cardSelector)
         .content
         .querySelector('.element')
         .cloneNode(true)
-        return card
+        return cardElement
     }
     _cardLike() {
         this._likeBtn.classList.toggle('element__like-active')
@@ -31,19 +31,18 @@ export class Card {
             this._cardDelete()
         })
         this._cardLink.addEventListener('click', () => {
-            this._handleCardClick(this._name, this._link)
+            this._handleCardClick(this._place, this._link)
         })
         this._likeBtn.addEventListener('click', () => {
             this._cardLike()
         })
-
     }
     generateCard() {
-        
-        this._cardName.textContent = this._name
-        this._cardLink.src = this._link
-        this._cardLink.alt = this._name
         this._setEventListeners()
+        this._cardPlace.textContent = this._place
+        this._cardLink.src = this._link
+        this._cardLink.alt = this._place
+        
         return this._card
     }
 }
