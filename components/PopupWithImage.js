75acd0f8b7b9af49d43c1e97b9f0c8913b0c4da3
Polina Ka <polinako1994@gmail.com>
2022-08-09 10:45:26 +0900

    Update PopupWithImage.js

--- components/PopupWithImage.js
+++ components/PopupWithImage.js
@@ -1,16 +1,16 @@
-import { Popup } from "./Popup.js";
+import {Popup} from "../components/Popup.js";
 
 export class PopupWithImage extends Popup {
   constructor(popupSelector) {
     super(popupSelector);
-    this._popupOpenImage = this._popup.querySelector('.popup-image__image')
-    this._popupOpenTitle = this._popup.querySelector('.popup-image__title')
+    this._figcaption = this._popupElement.querySelector('.popup-image__title');
+    this._image = this._popupElement.querySelector('.popup-image__image');
   }
 
-  open(place, link) {
+  open(data) {
     super.open();
-    this._popupOpenImage.alt = place
-    this._popupOpenImage.src = link
-    this._popupOpenTitle.textContent = place
+    this._image.src = data.link;
+    this._image.alt = data.name;
+    this._figcaption.textContent = data.name;
   }
 }
