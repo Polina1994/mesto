5c8377e8e815e4bd15a6bc46c81829f10b1f8365
Polina Ka <polinako1994@gmail.com>
2022-08-09 12:47:37 +0900

    Update UserInfo.js

--- components/UserInfo.js
+++ components/UserInfo.js
@@ -1,25 +1,24 @@
 export class UserInfo {
-  constructor(userInfo) {
-    this._profileName = document.querySelector(userInfo.nameSelector);
-    this._profileDescription = document.querySelector(userInfo.descriptionSelector);
-    this._profileId = null;
-    this._profileAvatar = document.querySelector(userInfo.profileAvatar);
+    constructor(userInfo) {
+      this._profileName = document.querySelector(userInfo.nameSelector);
+      this._profileDescription = document.querySelector(userInfo.descriptionSelector);
+      this._profileId = null;
+      this._profileAvatar = document.querySelector(userInfo.profileAvatar);
+    }
+  
+    getUserInfo() {
+      return {
+        name: this._profileName.textContent,
+        about: this._profileDescription.textContent,
+        profileId: this._profileId,
+        profileAvatar: this._profileAvatar.src
+      };
+    }
+  
+    setUserInfo(data) {
+      this._profileName.textContent = data.name;
+      this._profileDescription.textContent = data.about;
+      this._profileId = data.profileId;
+      this._profileAvatar.src = data.profileAvatar;
+    }
   }
-
-  getUserInfo() {
-    return {
-      name: this._profileName.textContent,
-      about: this._profileDescription.textContent,
-      profileId: this._profileId,
-      profileAvatar: this._profileAvatar.src
-    };
-  }
-
-  setUserInfo(data) {
-    this._profileName.textContent = data.name;
-    this._profileDescription.textContent = data.about;
-    this._profileId = data.profileId;
-    this._profileAvatar.src = data.profileAvatar;
-  }
-}
-
