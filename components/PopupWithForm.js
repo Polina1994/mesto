2eba69d1837a9a47b7d5f389123af31dcf00c829
Polina Ka <polinako1994@gmail.com>
2022-08-09 10:45:27 +0900

    Update PopupWithForm.js

--- components/PopupWithForm.js
+++ components/PopupWithForm.js
@@ -1,33 +1,42 @@
-import { Popup } from "./Popup.js";
+import {Popup} from "../components/Popup.js";
 
 export class PopupWithForm extends Popup {
-  constructor({handleFormSubmit}, popupSelector) {
+  constructor(popupSelector, submitFunction) {
     super(popupSelector);
-    this._handleFormSubmit = handleFormSubmit;
-    this._formElement = this._popup.querySelector(".popup__form");
-    this._formInputs = Array.from(this._formElement.querySelectorAll(".popup__input"));
+    this._submitFunction = submitFunction;
+    this._popupForm = this._popupElement.querySelector('.popup__form');
+    this._inputList = Array.from(this._popupForm.querySelectorAll('.popup__input'));
+    this._submitButton = this._popupForm.querySelector('[type="submit"]');
   }
-
   _getInputValues() {
-    const formValue = {}
-    this._formInputs.forEach((input) => {
-      formValue[input.name] = input.value;
+    const result = {};
+    this._inputList.forEach((inputElement) => {
+      result[inputElement.name] = inputElement.value;
     });
-
-    return formValue;
+    return result; 
   }
 
   setEventListeners() {
     super.setEventListeners();
-    this._formElement.addEventListener('submit', (evt) => {
-      evt.preventDefault()
-      this._handleFormSubmit(this._getInputValues())
+    this._popupForm.addEventListener('submit', (e) => {
+      e.preventDefault();
+      this._submitFunction(this._getInputValues());
     });
   }
 
   close() {
     super.close();
-    this._formElement.reset();
+    this._popupForm.reset();
+  }
+
+  setLoadingState(enabled) {
+    if (enabled) {
+      this._submitButton.textContent = 'Сохранение...'
+      this._submitButton.setAttribute('disabled', 'true');
+    } else {
+      this._submitButton.innerText = 'Сохранить'
+      if (this._submitButton.hasAttribute('disabled'))
+        this._submitButton.removeAttribute('disabled');
+    }
   }
 }
-
