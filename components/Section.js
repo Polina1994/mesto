fceed4e26aa9dd16fdcb30648c000ace17322998
Polina Ka <polinako1994@gmail.com>
2022-08-10 09:19:34 +0900

    Update Section.js

--- components/Section.js
+++ components/Section.js
@@ -11,7 +11,10 @@ export class Section {
     }
   
     addItem = (element) => {
-      this._container.prepend(element);
+      this._container.append(element);
+    }
+    addNewItem = (element) => {
+      this._container.prepend(element)
     }
   
   }
